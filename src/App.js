73acb1d51bd6e4c75f8ecd9faf7a4b720019fb71
AkshayPatil07695 <146464776+AkshayPatil076@users.noreply.github.com>
2024-04-28 08:23:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React from 'react'
-import './App.css'
+import Navbar from './page/Navbar'
+import Header from './page/Header'
+import Card from './page/Card'
+import About from './page/About'
+import Footter from './page/Footter'
+
 export default function App() {
   return (
     <div>
-   <div class="navbar">
-  <div class="navbar-logo">Your Logo</div>
-  <div class="navbar-links">
-    <div class="circle-text">Home</div>
-    <a href="#" class="navbar-link">About</a>
-    <a href="#" class="navbar-link">Services</a>
-    <a href="#" class="navbar-link">Portfolio</a>
-    <a href="#" class="navbar-link">Contact</a>
-  </div>
-  <button class="sign-in-btn">Sign In</button>
-  <button class="sign-up-btn">Sign Up</button>
-</div>
-
-
-<div class="header">
-  <div class="header-text">
-    <h1 class="animated-text">Website was created by using Chat GTP</h1>
-    <div class="bios-animated-text">This website was created by using Chat GTP-4 . As per the help of AI tool i create this website for demo website profile .</div>
- 
-  </div>
-  
-  <img src="https://cdni.iconscout.com/illustration/premium/thumb/business-congratulation-8867910-7269173.png" alt="Header Image" class="header-image" />
-</div>
+   <Navbar />
+    <Header />
+    <hr />
+    <Card />
+    <About />
+    <Footter />
 </div>
   )
 }
